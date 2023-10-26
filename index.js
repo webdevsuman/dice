a51c819f362058f8f8747f716689ea9b5206d1a6
Suman Das <40394572+mysticdevsuman@users.noreply.github.com>
2023-10-26 10:01:33 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,24 +1,19 @@
-var randomNumber1 = Math.floor(Math.random() *6) + 1; // random number from 1 to 6
+var randomNumber1 = Math.round(5 * Math.random() + 1);
 
-var randomDiceImage = "dice"+ randomNumber1 + ".png"; // dice1.png - dice6.png
+var randomNumber2 = Math.round(5 * Math.random() + 1);
 
-var randomImageSource = "images/" + randomDiceImage; // images/dice1.png - images/dice6.png
+document
+  .querySelector(".img1")
+  .setAttribute("src", "images/dice" + randomNumber1 + ".png");
 
-var image1= document.querySelectorAll("img")[0];
-image1.setAttribute("src",randomImageSource);
+document
+  .querySelector(".img2")
+  .setAttribute("src", "images/dice" + randomNumber2 + ".png");
 
-var randomNumber2 = Math.floor(Math.random()*6) + 1;
-
-var randomImageSource2 = "images/dice" + randomNumber2 + ".png";
-
-var image2= document.querySelectorAll("img")[1];
-
-image2.setAttribute("src",randomImageSource2);
-
-if (randomNumber1>randomNumber2) {
-	document.querySelector("h1").innerHTML="Player 1 Wins!";
+if (randomNumber1 > randomNumber2) {
+  document.querySelector("h1").innerHTML = "🚩 Player 1 Wins!";
+} else if (randomNumber1 < randomNumber2) {
+  document.querySelector("h1").innerHTML = "Player 2 Wins! 🚩";
+} else {
+  document.querySelector("h1").innerHTML = "🚩DRAW!🚩";
 }
-else if (randomNumber2>randomNumber1) {
-	document.querySelector("h1").innerHTML="Player 2 Wins!";
-}
-else{document.querySelector("h1").innerHTML="Draw!";}
